b7bcc66a6b8f9291518b9b57b33cf43133f3b981
mhs121 <62454499+mhs121@users.noreply.github.com>
2020-04-12 14:54:13 +0800

    Buat ClassOperasiString

--- src/OpersiString.java
+++ src/OpersiString.java
@@ -4,5 +4,9 @@ public class OpersiString {
     public static void main(String[] args) {
         String kota = "Banjarmasin";
         System.out.println(kota);
+
+        char[]uniskaChar = {"U","N","I","S","K","A"};
+        String uniskaString = new String(uniskaChar);
+        System.out.println(uniskaString);
     }
 }
